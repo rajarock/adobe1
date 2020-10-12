b2c2ea553c40f7a43e363cc33459d62c1090aca6
rajarock <rajaflexible@gmail.com>
2020-10-12 18:52:08 +0530

    fix prod changes

--- config/development.js
+++ config/development.js
@@ -9,22 +9,6 @@ const express = require('express');
 const favicon = require('express-favicon');
 
 const port = process.env.PORT || 8080;
-
-
-function server() {
-    const app = express();
-    app.use(favicon(__dirname + '/public/favicon.png'));
-    // the __dirname is the current directory from where the script is running
-    app.use(express.static(__dirname));
-    // send the user to index html page inspite of the url
-    app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
-    });
-    app.listen(port);
-}
-
-
-
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const isProd = process.env.NODE_ENV === 'production'
 
@@ -35,13 +19,12 @@ function init(){
         stats: "errors-only"
     })
     webpackServer.listen(port,'localhost',()=>console.log(
-        `Server listening on \x1b[42m\x1b[1mhttp://localhost:${process.env.PORT}\x1b[0m in \x1b[41m\x1b[0m `
+        `Server listening on \x1b[42m\x1b[1mhttp://localhost:${port}\x1b[0m in \x1b[41m\x1b[0m `
       ))
 }
 
 if(isProd)
 {
-    // server()
     webpack([webpackCommon]).run((err, stats) => {
         console.log('error message ',err, '\n stats message',stats)
     })

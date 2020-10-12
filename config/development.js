f9868780df0fa1f9194d8f47854a87f1fe59a312
rajarock <rajaflexible@gmail.com>
2020-10-12 15:15:53 +0530

    fix added server

--- config/development.js
+++ config/development.js
@@ -5,10 +5,27 @@ const webpackDevServer = require('webpack-dev-server')
 const webpackCommon = require('./webpack-common')
 const webpackDev = require('./webpack-dev')
 
+const express = require('express');
+const favicon = require('express-favicon');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+function server() {
+    app.use(favicon(__dirname + '/public/favicon.png'));
+    // the __dirname is the current directory from where the script is running
+    app.use(express.static(__dirname));
+    // send the user to index html page inspite of the url
+    app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+    });
+    app.listen(port);
+}
+
+
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const isProd = process.env.NODE_ENV === 'production'
-process.env.PORT = process.env.PORT || 5000
 
 function init(){
     const compiler = webpack(webpackDev)
@@ -16,15 +33,17 @@ function init(){
         historyApiFallback : true,
         stats: "errors-only"
     })
-    webpackServer.listen(process.env.PORT,'localhost',()=>console.log(
+    webpackServer.listen(port,'localhost',()=>console.log(
         `Server listening on \x1b[42m\x1b[1mhttp://localhost:${process.env.PORT}\x1b[0m in \x1b[41m\x1b[0m `
       ))
 }
+
 if(isProd)
 {
     webpack([webpackCommon]).run((err, stats) => {
         console.log('error message ',err, '\n stats message',stats)
     })
+    server()
 }
 else {
     init()

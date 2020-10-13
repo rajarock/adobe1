81e5229905b0d3fea496685694cf41ff7a370cb8
rajarock <rajaflexible@gmail.com>
2020-10-13 11:25:32 +0530

    fix

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-console.log({__dirname})
-app.use(favicon(__dirname + '/public/favicon.png'));
+// console.log({__dirname})
+// app.use(favicon(__dirname + './public/favicon.png'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 // send the user to index html page inspite of the url
